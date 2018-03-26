0aa6539c374afbd302d490f2520c93d5f159a8eb
Orrequia <orrequia8@gmail.com>
2018-03-26 18:00:16 +0200

    Backend eliminado directorios antiguos

--- Backend/ConTask/veryoldModels/message.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-
-var messageSchema = new Schema({
-    text: String
-});
-
-module.exports = mongoose.model('Message', messageSchema);
